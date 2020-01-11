dff9aaae63bbff0e8abaad1b8e9f3941a06b2a8e
geordie-quiroa <gqmail@protonmail.com>
2020-01-11 17:00:48 -0600

    optimizacion conversion a int

--- adyacentes/adysum.py
+++ adyacentes/adysum.py
@@ -3,6 +3,8 @@
 
 def procesar(path2file):
     lineas = open(path2file,'r').read().split('\n')
-    matriz_limpia = [linea.split() for linea in lineas if len(linea.split()) > 0]
+    matriz_limpia = [list(map(int,linea.split())) for linea in lineas if len(linea.split()) > 0]
     for linea in matriz_limpia:
-        print(linea)
+        linea = list(map(int,linea))
+        #print(linea)
+    print(matriz_limpia)
