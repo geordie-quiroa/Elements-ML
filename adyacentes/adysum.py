2fe7103ca3365ab9b33a4f96ee557e34083a4817
geordie-quiroa <gqmail@protonmail.com>
2020-01-11 17:57:30 -0600

    suma adyacentes

--- adyacentes/adysum.py
+++ adyacentes/adysum.py
@@ -3,8 +3,32 @@
 
 def procesar(path2file):
     lineas = open(path2file,'r').read().split('\n')
-    matriz_limpia = [list(map(int,linea.split())) for linea in lineas if len(linea.split()) > 0]
-    for linea in matriz_limpia:
-        linea = list(map(int,linea))
-        #print(linea)
-    print(matriz_limpia)
+    arbol_limpio = [list(map(int,linea.split())) for linea in lineas if len(linea.split()) > 0]  # omite lineas vacias y convierte cada elemento a entero
+    print(arbol_limpio)
+    _recorrer(arbol_limpio)
+
+def _adyacente(arreglo, indice_anterior, total_acum) -> list:
+    max_len = len(arreglo)
+    inf, sup = 0, max_len
+    if indice_anterior != 0 and max_len > 3 :
+        inf, sup = indice_anterior - 1, indice_anterior + 2 # + 2 porque el slicing de arreglos es excluyente [inf:sup)
+    elif indice_anterior == 0 and max_len > 2:
+        sup = indice_anterior + 2
+        #sup = max_len
+    total_acum += max(arreglo[inf:sup])
+    idx = arreglo.index(max(arreglo[inf:sup]))
+
+    
+    return (idx,total_acum)
+
+def _recorrer(arbol):
+    total = 0
+    for i in range(len(arbol)): # recorre cada linea del archivo
+        if i == 0:
+            total += arbol[0][0]
+            idx = 0
+        else:
+            idx, total = _adyacente(arbol[i], idx, total)
+        #adyacente(nivel)
+    print(total)
+
--- adyacentes/main.py
+++ adyacentes/main.py
@@ -2,3 +2,12 @@ import adysum
 path = "./testfile.txt"
 
 adysum.procesar(path)
+# arreglo = [1, 2, 3]
+# inf = 0
+# sup = len(arreglo)
+# total_acum = 10
+# idx = 0
+# #print(arreglo[0:1])
+# total_acum += max(arreglo[inf:sup])
+# idx = arreglo.index(max(arreglo[inf:sup]))
+# print(total_acum)
