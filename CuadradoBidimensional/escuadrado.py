bd139b4f6249118d19ea74a2cd916cac9ef69062
geordie-quiroa <gqmail@protonmail.com>
2020-01-11 22:44:12 -0600

    Bug de nueva linea arreglado

--- CuadradoBidimensional/escuadrado.py
+++ CuadradoBidimensional/escuadrado.py
@@ -7,7 +7,7 @@ def cuadrado(tuplas):
         _abscisas.append(x)
         _ordenadas.append(y)
 
-    if (len(set(_abscisas)) == len(set(_ordenadas)))\ 
+    if (len(set(_abscisas)) == len(set(_ordenadas)))\
         and (max(set(_abscisas))-min(set(_abscisas))) == (max(set(_ordenadas))-min(set(_ordenadas))):
         return ("Las coodenadas si forman un cuadrado.")
     else:
