eb711c96fcfc4652b63bb0f1140ea980494eb2db
geordie-quiroa <gqmail@protonmail.com>
2020-01-11 18:12:24 -0600

    docstrings

--- adyacentes/adysum.py
+++ adyacentes/adysum.py
@@ -9,7 +9,7 @@ Retorna un string con el resultado de la sumatoria.
 
 def procesar(path2file) -> str:
     """Args
-    Path de un archivo .txt cuyos digitos formen un triangulo. 
+    path2file -- Path de un archivo .txt cuyos digitos formen un triangulo. 
     """
     lineas = open(path2file,'r').read().split('\n')
     arbol_limpio = [list(map(float,linea.split())) for linea in lineas if len(linea.split()) > 0]  # omite lineas vacias y convierte cada elemento a digito
@@ -20,11 +20,10 @@ def procesar(path2file) -> str:
 def _adyacente(arreglo, indice_anterior, total_acum) -> list:
     max_len = len(arreglo)
     inf, sup = 0, max_len
-    if indice_anterior != 0 and max_len > 3 :
+    if indice_anterior != 0 and max_len > 3: 
         inf, sup = indice_anterior - 1, indice_anterior + 2 # + 2 porque el slicing de arreglos es excluyente [inf:sup)
     elif indice_anterior == 0 and max_len > 2:
         sup = indice_anterior + 2
-        #sup = max_len
     total_acum += max(arreglo[inf:sup])
     idx = arreglo.index(max(arreglo[inf:sup]))
     return (idx,total_acum)
