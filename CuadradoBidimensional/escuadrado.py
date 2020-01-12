dc36f4d8bae96b9f9c201c4a0520cdff5e7fe0f8
geordie-quiroa <gqmail@protonmail.com>
2020-01-11 22:42:52 -0600

    comparaciones listas

--- CuadradoBidimensional/escuadrado.py
+++ CuadradoBidimensional/escuadrado.py
@@ -4,10 +4,11 @@ def cuadrado(tuplas):
     _ordenadas = []
     for tupla in tuplas:
         x,y = tupla
-        _abscisas.append(abs(x))
-        _ordenadas.append(abs(y))
+        _abscisas.append(x)
+        _ordenadas.append(y)
 
-    if len(set(_abscisas)) == len(set(_ordenadas)):
-        return ("Si es cuadrado.")
+    if (len(set(_abscisas)) == len(set(_ordenadas)))\ 
+        and (max(set(_abscisas))-min(set(_abscisas))) == (max(set(_ordenadas))-min(set(_ordenadas))):
+        return ("Las coodenadas si forman un cuadrado.")
     else:
-        return ("No es cuadrado.")
+        return ("Las coordenadas no forman un cuadrado.")
--- CuadradoBidimensional/main.py
+++ CuadradoBidimensional/main.py
@@ -1,10 +1,20 @@
 from escuadrado import cuadrado
 
 coordenadas = [
-                (3,3),
-                (6,-1),
-                (6,3),
-                (3,-1)
-            ]
+            (2,3),
+            (6,-1),
+            (6,3),
+            (2,-1)
+        ]
 
 print(cuadrado(coordenadas))
+
+# _abscisas = []
+# _ordenadas = []
+# for tupla in tuplas:
+#     x,y = tupla
+#     _abscisas.append(x)
+#     _ordenadas.append(y)
+
+# print(max(set(_abscisas)) - min(set(_abscisas)))
+# print(max(set(_ordenadas)) - min(set(_ordenadas)))
