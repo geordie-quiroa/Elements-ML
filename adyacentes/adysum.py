34a60132a0bf787bcd50922936eddbec4857725c
geordie-quiroa <gqmail@protonmail.com>
2020-01-11 23:04:34 -0600

    docs

--- adyacentes/adysum.py
+++ adyacentes/adysum.py
@@ -20,9 +20,9 @@ def procesar(path2file) -> str:
 def _adyacente(arreglo, indice_anterior, total_acum) -> list:
     max_len = len(arreglo)
     inf, sup = 0, max_len
-    if indice_anterior != 0 and max_len > 3: 
-        inf, sup = indice_anterior - 1, indice_anterior + 2 # + 2 porque el slicing de arreglos es excluyente [inf:sup)
-    elif indice_anterior == 0 and max_len > 2:
+    if indice_anterior != 0 and max_len > 3:  # caso en el que la linea tiene mas de 3 numeros, se puede indexar hacia ambos lados. 
+        inf, sup = indice_anterior - 1, indice_anterior + 2  # + 2 porque el slicing de arreglos es excluyente [inf:sup)
+    elif indice_anterior == 0 and max_len > 2:  # caso en el que el adyacente no tiene frontera izquierda
         sup = indice_anterior + 2
     total_acum += max(arreglo[inf:sup])
     idx = arreglo.index(max(arreglo[inf:sup]))
