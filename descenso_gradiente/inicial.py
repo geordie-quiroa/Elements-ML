f6953a5d7834af3ae793522d7db85f6bc6f8288c
geordie-quiroa <gqmail@protonmail.com>
2020-01-29 13:48:01 -0600

    overflow arreglado

--- descenso_gradiente/inicial.py
+++ descenso_gradiente/inicial.py
@@ -3,75 +3,45 @@ from random import seed
 import matplotlib.pyplot as plt
 
 seed(1)
-
-#teta_1 = ((random() - 0.5) * 2 * desv * random() + x  )
-
-#desv = 0.5
-
-#mu = 2
-
-#teta_original_1 = ((random() - 0.5) * 2 * desv * random() + mu)
-
-#teta_original_0 = 5
-
-# fx = lambda x: teta_original_0 + teta_original_1*x + ((random() - 0.5) * 2 * desv * random() + mu)*1.5 if x % 2 == 0 \
-#     else teta_original_0 + teta_original_1*x - ((random() - 0.5) * 2 * desv * random() + mu)*1.5
-
 bias = 25
 varianza = 50
+
 fx = lambda x: (x + bias) + uniform(0,1) * varianza
 
 xs = [i for i in range(100)]
 ys = [fx(x) for x in xs]
 
-#h_teta = teta_0 + teta_1*x
 m = len(xs) 
-#j = (y - h_teta)**2
 
 plt.scatter(xs, ys)
 plt.show()
 
-h_teta = lambda teta_0, teta_1, x: teta_0 + teta_1 * x
+n_steps = 100
+alpha = 0.10
+criteria = 0.008
+teta_0 = 1
+teta_1 = 1
 
-#error_cuadrado = [(h_teta(teta_0, teta_1, i) - fx(i))**2 for i in xs]
+convergido = False
+n = 0
 
-#j = lambda teta_0, teta_1: 1 * (2*m)**-1 * sum([(fx(xi) - h_teta(teta_0, teta_1, xi))**2 for xi in xs])
-j = lambda teta_0, teta_1: (sum([(fx(xi) - h_teta(teta_0, teta_1, xi))**2 for xi in xs]) / (2*m))
+while not convergido:
+    n+=1
+    teta_0_gradient = 0
+    teta_1_gradient = 0
+    m = float(len(xs))
 
-# def jota(teta_0, teta_1):
-#    return 1 / 2*m * sum(error_cuadrado)
-#print(h_teta(teta_0, teta_1, 1))
-#print(error_cuadrado[:5])
-
-def descenso_gradiente(alpha, n_iteraciones, tol):
-    n=1
-    teta_0 = 1
-    teta_1 = 1
-    convergido = False
-    error_old = j(teta_0, teta_1)    
-
-    while not convergido:
-        n+=1
-        print(n)
-        print(error_old)
-
-        #teta_0 -=  alpha * 1 * (m)**-1 * sum([(fx(xi) - h_teta(teta_0, teta_1, xi)) for xi in xs])
-        teta_0 -=  alpha * (sum([(fx(xi) - h_teta(teta_0, teta_1, xi)) for xi in xs]) / m)
-        #teta_1 -= alpha * 1 * (m)**-1 * sum([(fx(xi) - h_teta(teta_0, teta_1, xi))*xi for xi in xs])
-        teta_1 -= alpha * (sum([(fx(xi) - h_teta(teta_0, teta_1, xi))*xi for xi in xs]) / m)
-
-        error = j(teta_0, teta_1)
-        #print(error)
-        #if abs(j(teta_0, teta_1) - j(teta_0_old, teta_1_old)) < 0.005:
-        if abs(error - error_old) <= tol:
-            convergido = True
+    for i in range(0, len(xs)):
+        teta_0_gradient += (ys[i] - (teta_0 + teta_1 * xs[i]))
+        teta_1_gradient += (ys[i] - (teta_0 + teta_1 * xs[i])) * xs[i]
         
-        if n > n_iteraciones:
-            print("Maximo de iteraciones alcanzado.")
-            convergido = True
+    teta_0 -= (alpha * (teta_0_gradient / m))
+    teta_1 -= (alpha * (teta_1_gradient / m))
 
-        error_old = error
-    return (teta_0, teta_1)
-
-
-print(descenso_gradiente(0.25,100,0.0001))
+    if max(abs(alpha * teta_0_gradient), abs(alpha * teta_1_gradient)) < criteria:
+        convergido = True
+    elif n >= n_steps:
+        print("No converge, pero se llego a las maximas iteraciones")
+        convergido = True
+    
+print("Los valores que se obtienen son: {}, {} en {} pasos.".format(teta_0, teta_1, n))
