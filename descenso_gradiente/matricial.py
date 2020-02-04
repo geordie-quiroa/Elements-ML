c04aea6e0940e11c5cce8e4c017dae805f80b848
geordie-quiroa <gqmail@protonmail.com>
2020-02-04 15:57:58 -0600

    problemas de shapes arreglados

--- descenso_gradiente/matricial.py
+++ descenso_gradiente/matricial.py
@@ -17,8 +17,7 @@ fx = lambda x: (x + bias) + uniform(0,1) * varianza
 n_obs = 100
 
 # arreglos para datos en eje x, y.
-xs = [i for i in range(n_obs)]
-ys = [fx(x) for x in xs]
+xs = np.array([i for i in range(n_obs)])
 unos = np.ones(n_obs)
 
 X = np.vstack(
@@ -30,7 +29,15 @@ print(X[:10,:])
 print(X.shape)
 
 m, n = X.shape
+
+ys = np.array([fx(x) for x in xs])
+ys = ys.reshape(m,1) # convertir a vector columna
+
 tetas = np.random.rand(2,1)
 
-h = np.matmul(X, tetas) # vector solucion
+h = np.matmul(X, tetas) # vector solucion (100,1)
+print((h - ys).shape) # (100,1) - (100,1)
+alpha = 0.01
+
+tetas = tetas - alpha * (np.matmul((h - ys).T, X).T / m) # (1,100) * (100, 2) . T = (2, 1) 
 print(tetas)
