f974ae5b8f0c8e0df5abd839ee90bc487ea92eb4
geordie-quiroa <gqmail@protonmail.com>
2020-02-04 15:06:30 -0600

    nube de puntos creada

---
+++ descenso_gradiente/matricial.py
@@ -0,0 +1,18 @@
+from random import random, uniform, seed
+import matplotlib.pyplot as plt
+
+
+""" Generacion de los datos
+- xs = lista de valores en x.
+- ys = lista de valores para fx.
+"""
+seed(1)
+
+bias = 25
+varianza = 50
+
+fx = lambda x: (x + bias) + uniform(0,1) * varianza
+
+# arreglos para datos en eje x, y.
+xs = [i for i in range(100)]
+ys = [fx(x) for x in xs]
